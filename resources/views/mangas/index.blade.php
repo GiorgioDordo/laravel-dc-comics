43ce0b7760152f0184f0b27b96d4d379f2129c35
GiorgioDordo <djole6789@gmail.com>
2024-10-29 17:54:01 +0100

    printed datas on Dom

--- resources/views/mangas/index.blade.php
+++ resources/views/mangas/index.blade.php
@@ -3,5 +3,34 @@
 @section("page-title", "Mangas")
 
 @section("main-content")
-<h1>Mangas</h1>
+<section class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center">
+                MANGAS
+            </h1>
+        </div>
+        <div class="row d-flex justify-content-center">
+            @foreach ($mangas as $manga)
+            <div class="col-3 m-3 p-3 d-flex flex-column align-items-center  border rounded">
+                <div class="manga-cover">
+                    <img class="border card-img-top" src="{{$manga["thumbs"]}}" alt="icon">
+                </div>
+                <div class="card-body text-center d-flex flex-column justify-content-end mt-2">
+                    <p>
+                        <strong>{{$manga["title"]}}</strong>
+                    </p>
+                    <p>
+                        {{$manga["publisher"]}}
+                    </p>
+                    <p>
+                        {{$manga["price"]}} €
+                    </p>
+                    <button type="button" class="btn btn-light"><strong>Aggiungi al carrello</strong></button>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MangaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[HomeController::class, 'home'])->name('home');
+Route::get('/mangas', [MangaController::class, 'index'])->name('manga');
